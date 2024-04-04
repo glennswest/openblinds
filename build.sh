08a27304d756b00c3769625ce4df9a7e1d2eb706
Glenn West <gwest@redhat.com>
2024-04-04 17:40:54 -0500

    Add js based web

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-./bin/mklittlefs -s 194560 -c data littlefs.bin
+./bin/mklittlefs -s 1572864  -c data littlefs.bin
 esptool.py --chip esp32c3 --port /dev/cu.usbmodem101 --baud 921600 write_flash 0x290000 littlefs.bin -u  
 
